935b8dafc0c8ccc9ca3a8942a9ff71315eb44a2d
wooorim <woorim0923@gmail.com>
2023-04-17 12:00:44 +0900

    spring security first commit

---
+++ src/main/java/edu/global/ex/SpringBootMwrSecurityApplication.java
@@ -0,0 +1,13 @@
+package edu.global.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMwrSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMwrSecurityApplication.class, args);
+	}
+
+}
